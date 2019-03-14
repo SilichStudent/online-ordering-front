e0fd78ab20f27309e8510e546a4c1abc3ffce7b0
s.silich <s.silich@softarex.com>
2019-03-14 12:05:47 +0300

    comit

--- src/common/constants/index.js
+++ src/common/constants/index.js
@@ -1,6 +1,8 @@
+import { API_ULR as apiUrl } from '../../../config/config'
+
 export const USER_TOKEN = "USER_TOKEN";
 export const MANAGER_TOKEN = "MANAGER_TOKEN";
 
 export const AUTH_TOKEN = "Auth-Token";
 
-export const API_ULR = "http://localhost:3001/api/v1"
+export const API_ULR = apiUrl
